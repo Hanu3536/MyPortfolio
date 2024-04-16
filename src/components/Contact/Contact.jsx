f06c1dfc1fbbb503e96a4e61ad07996456fe3f22
Hanumantharao Chadalavada <hanuca3536@gmail.com>
2024-04-16 00:31:24 -0700

    first commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2><br/><br/>
+        <a href="https://www.linkedin.com/in/naga-h-chadalavada" className={styles.contactBtn}>
+          Let's Connect
+        </a>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:hanuca3536@gmail.com">hanuca3536@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/naga-h-chadalavada">linkedin.com/in/naga-h-chadalavada</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://www.github.com/myname">github.com/myname</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./Intro.module.css";
+import { getFileUrl, getImageUrl } from "../../utils";
+
+
+export const Intro = () => {
+  
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I am Naga H Chadalavada</h1>
+        <p className={styles.description}>
+          Experienced frontend developer specialized in creating dynamic responsive websites that prioritize user experience.
+        </p>
+        <a href={getFileUrl('hero/Resume-Naga Ch.pdf')} alt = "" className={styles.contactBtn}>
+          Resume
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
